80aa5e8ef4a9efa76cfb1fdad06d8f374f17b8e8
Charlotte Goldthwaite <chargold201@gmail.com>
2019-06-11 10:59:32 -0400

    Add looping and edge cases to CLI

--- lib/fresh_tomatoes/cli.rb
+++ lib/fresh_tomatoes/cli.rb
@@ -3,6 +3,18 @@ class FreshTomatoes::CLI
         self.greeting
         self.list_movies
         self.pick_movie
+        loop do
+            puts "\nWould you like to view information on another movie?"
+            input = gets.strip.downcase
+            if input == "y" || input == "yes"
+                self.pick_movie
+            elsif input == "n" || input == "no"
+                puts "\nThank you, good bye!"
+                break
+            else
+                puts "\nThat is not a valid response."
+            end
+        end
     end
 
     def greeting
@@ -18,16 +30,21 @@ class FreshTomatoes::CLI
     def pick_movie
         puts "\nPlease enter the number of the movie you are interested in:"
         input = gets.strip.to_i
-        movie = FreshTomatoes::Movie.all[input-1]
-        FreshTomatoes::Scraper.scrape_details(movie)
-        display_movie_details(movie)
+        if input.between?(1,FreshTomatoes::Movie.all.length)
+            movie = FreshTomatoes::Movie.all[input-1]
+            FreshTomatoes::Scraper.scrape_details(movie)
+            display_movie_details(movie)
+        else
+            puts "\nSorry, that is not a valid option."
+        end
     end
 
     def display_movie_details(movie)
         puts "\n-#{movie.title}-"
-        puts "Description: #{movie.description}"
+        puts "Tomatometer: #{movie.tomatometer}"
         puts "Audience Score: #{movie.audience_score}"
         puts "Rating: #{movie.rating}"
         puts "Runtime: #{movie.runtime}"
+        puts "Description: #{movie.description}"
     end
 end
